30526072bed42c644e83e4ea49c98b501fb5fb3d
Eliot Hertenstein <39346975+eiiot@users.noreply.github.com>
2024-02-09 10:29:29 -0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Link href="https://rmss.bhs.sh">🍎 rmss.bhs.sh</Link>
+      <Link href="https://testing.bhs.sh">🎓 testing.bhs.sh</Link>
+      <Link href="https://fire.bhs.sh">🔥 fire.bhs.sh</Link>
+      <Link href="https://ibgrades.bhs.sh">📊 ibgrades.bhs.sh</Link>
+      <Link href="https://schedules.bhs.sh">🗓️ schedules.bhs.sh</Link>
+      <Link href="https://pomodoro.bhs.sh">🕰️ countdown.bhs.sh</Link>
+      <Link href="https://auth.bhs.sh">🔒 auth.bhs.sh</Link>
+      <Link href="https://ahc.bhs.sh">🏛️ ahc.bhs.sh</Link>
+      <span className="cursor-wait">
+        ⁉️ <span className="blur-sm">comingsoon</span>
+        .bhs.sh
+      </span>
+      <span className="!mt-4">
+        created with ❤️ by{" "}
+        <Link href="https://eliothertenstein.com" className="underline">
+          eliot
+        </Link>
+      </span>
+    </main>
+  );
+}
