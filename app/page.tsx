cc47befe27572c6e582eda634ff248a9ee18e991
Eliot Hertenstein <39346975+eiiot@users.noreply.github.com>
2024-04-12 09:40:13 -0700

    update list!

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Link href="https://ivy.bhs.sh">📍 findmy.bhs.sh</Link>
       <Link href="https://rmss.bhs.sh">🍎 rmss.bhs.sh</Link>
       <Link href="https://testing.bhs.sh">🎓 testing.bhs.sh</Link>
       <Link href="https://calc.bhs.sh">🪴 calc.bhs.sh</Link>
@@ -10,7 +11,6 @@ export default function Home() {
       <Link href="https://ibgrades.bhs.sh">📊 ibgrades.bhs.sh</Link>
       <Link href="https://schedules.bhs.sh">🗓️ schedules.bhs.sh</Link>
       <Link href="https://countdown.bhs.sh">🕰️ countdown.bhs.sh</Link>
-      <Link href="https://ivy.bhs.sh">🌿 ivy.bhs.sh</Link>
       <Link href="https://ivyday.bhs.sh">🎓 ivyday.bhs.sh</Link>
       <Link href="https://map.bhs.sh">🗺️ map.bhs.sh</Link>
       <Link href="https://auth.bhs.sh">🔒 auth.bhs.sh</Link>
